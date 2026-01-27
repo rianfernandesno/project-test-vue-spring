cf32a6b19d906ef1befc54437ae09617068b54c0
rianfernandesno <zrpapz12@gmail.com>
2026-01-27 11:16:02 -0300

    first commit

---
+++ backend/src/main/java/com/yui/asvum_api/AsvumApiApplication.java
@@ -0,0 +1,13 @@
+package com.yui.asvum_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsvumApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsvumApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/yui/asvum_api/AsvumApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yui.asvum_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsvumApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
