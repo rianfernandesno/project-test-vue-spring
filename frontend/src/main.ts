a8a432d390dc1a24f1761014419f38509899dc56
rianfernandesno <zrpapz12@gmail.com>
2026-01-27 15:23:08 -0300

    Configurações gerais de CSS, APP e main

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,28 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+// main.ts
+import { createApp } from 'vue';
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
+import App from './App.vue';
+import router from './router';
+import './style.css';
 
-createApp(App).mount('#app')
+// Importe os componentes que você usou
+import Menubar from 'primevue/menubar';
+import Button from 'primevue/button';
+import Menu from 'primevue/menu';
+
+const app = createApp(App);
+
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+});
+
+// Registre-os globalmente
+app.component('Menubar', Menubar);
+app.component('Button', Button);
+app.component('Menu', Menu);
+
+app.use(router);
+app.mount('#app');
