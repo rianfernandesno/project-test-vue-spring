d7c001bf53915bb766289791516a2dd2eda6eecc
rianfernandesno <zrpapz12@gmail.com>
2026-02-02 15:58:53 -0300

    optmizie

--- backend/src/main/java/com/yui/asvum_api/repository/BarbeiroRepository.java
+++ backend/src/main/java/com/yui/asvum_api/repository/BarbeiroRepository.java
@@ -1,12 +1,18 @@
 package com.yui.asvum_api.repository;
 
+import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.yui.asvum_api.models.entities.Barbeiro;
 
 public interface BarbeiroRepository  extends JpaRepository<Barbeiro, Long>{
+
+     @Override
+     @EntityGraph(attributePaths = {"especialidades", "horariosTrabalho"})
+     List<Barbeiro> findAll();
 
      Optional<Barbeiro> findByNome(String name);
 }
