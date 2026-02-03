94216d325108f50f80d9f752a020adb6d265bb73
rianfernandesno <zrpapz12@gmail.com>
2026-02-03 17:42:08 -0300

    role and usuario repository

---
+++ backend/src/main/java/com/yui/asvum_api/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.yui.asvum_api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.yui.asvum_api.models.entities.Role;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {}  
+    
+
